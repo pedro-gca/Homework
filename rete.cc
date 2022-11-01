1d32f0d3799f00c2bff8e06900f55f1c7635752f
SimoneParrella <117114134+SimoneParrella@users.noreply.github.com>
2022-11-01 11:16:22 +0100

    Update rete.cc

--- rete.cc
+++ rete.cc
@@ -1,19 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 #include "ns3/applications-module.h"
 #include "ns3/core-module.h"
 #include "ns3/csma-module.h"
@@ -47,37 +31,33 @@ main(int argc, char* argv[])
     NodeContainer p2pnodes;
     p2pnodes.Add(csmaNodes.Get(2));
     p2pnodes.Create(1);
+    
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("25Mbps"));
+    csma.SetChannelAttribute("Delay", StringValue("10us"));
 
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue("15us"));
+    
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install(csmaNodes);
 
- /*  NetDeviceContainer devices;
-    devices = pointToPoint.Install(nodes);
+    NetDeviceContainer p2pdevices;
+    p2pdevices = pointToPoint.Install(p2pnodes);
 
     InternetStackHelper stack;
-    stack.Install(nodes);
-
+    stack.Install(csmaNodes);
+    stack.Install(p2pnodes);
+    
     Ipv4AddressHelper address;
+    address.SetBase("192.128.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer csmaInterfaces;
+    csmaInterfaces = address.Assign(csmaDevices);
     address.SetBase("10.1.1.0", "255.255.255.0");
-
-    Ipv4InterfaceContainer interfaces = address.Assign(devices);
-
-    UdpEchoServerHelper echoServer(9);
-
-    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
-    serverApps.Start(Seconds(1.0));
-    serverApps.Stop(Seconds(10.0));
-
-    UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
-    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
-    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
-
-    ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
-    clientApps.Start(Seconds(2.0));
-    clientApps.Stop(Seconds(10.0));
-    */
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign(p2pdevices);
+ 
      
 
     Simulator::Run();
